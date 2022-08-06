f810e2298e3e02b7cd77622b425bfaa3e8957c60
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-08-06 13:57:17 +0200

    ex2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,17 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const dateEl = document.querySelector('#datetime-picker');
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
-const bntEl = document.querySelector('[data-start]');
 
-bntEl.addEventListener('click', onStart);
-bntEl.disabled = true;
+const varDays = document.querySelector('[data-days]');
+const varHours = document.querySelector('[data-hours]');
+const varMinutes = document.querySelector('[data-minutes]');
+const varSeconds = document.querySelector('[data-seconds]');
+
+
+
+const bntStart = document.querySelector('[data-start]');
+bntStart.addEventListener('click', onStart);
+bntStart.disabled = true;
 
 const options = {
   enableTime: true,
@@ -21,13 +23,13 @@ const options = {
     // console.log(selectedDates[0]);
     if (selectedDates[0] < Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future');
-      bntEl.disabled = true;
+      bntStart.disabled = true;
     } else {
-      bntEl.disabled = false;
+      bntStart.disabled = false;
     }
   },
 };
-
+const dateEl = document.querySelector('#datetime-picker');
 flatpickr(dateEl, options);
 
 function onStart() {
@@ -36,10 +38,10 @@ function onStart() {
     const delta = selectedDate - Date.now();
     const { days, hours, minutes, seconds } = convertMs(delta);
 
-    daysEl.textContent = addLeadingZero(days);
-    hoursEl.textContent = addLeadingZero(hours);
-    minutesEl.textContent = addLeadingZero(minutes);
-    secondsEl.textContent = addLeadingZero(seconds);
+    varDays.textContent = addLeadingZero(days);
+    varHours.textContent = addLeadingZero(hours);
+    varMinutes.textContent = addLeadingZero(minutes);
+    varSeconds.textContent = addLeadingZero(seconds);
 
     if (delta <= 999) {
       clearInterval(timerId);
@@ -47,24 +49,24 @@ function onStart() {
   }, 1000);
 }
 
-function convertMs(delta) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(delta / day);
-  // Remaining hours
-  const hours = Math.floor((delta % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((delta % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((delta % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
